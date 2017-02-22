8436a0c9991d0d948a16f4529065fbbba0896e6e
royredman4 <royredman4@gmail.com>
2017-02-22 09:29:57 -0800

    Updated for future iterations

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,220 +1,73 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "deck.h"
-#include "card.h"
-#include <vector>
+//#include "deck.h"
+//#include "card.h"
+//#include "hand.h"
+//#include <vector>
+#include <QLabel>
+#include <QString>
+#include <QInputDialog>
+
+QList<QLabel*> Images;
+int free_index = 0;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
-
-
     ui->setupUi(this);
 
-// If you want to run this as a console program, uncomment everthing
-// from this line until line 164. Also make sure to comment
-// everything from main.cpp from line 29 to the return statement
-/*
-    vector<Card> Deck;
-    vector<Card> Hand;
-    vector<Card> DealerHand;
-    vector<Card> Graveyard;
+    for (int i = 0; i < 52; ++i) {
+        Images.push_back(new QLabel(this));
+    }
 
-    deck d;
+    Title_Message = "BlackJack Game: Version 1.0";
+    this->setWindowTitle(Title_Message);
 
-    bool end=false; //will end the game when true
+    //Asks for the number of players playing
+    int value = 1;
+    do {
+        value = QInputDialog::getInt(this, "Player Counter", "Please enter the number of players currently playing(Maximum 5)");
+       } while(value < 1 || value > 5);
 
+    current_player_number = 0;
+    current_hand_number = 0;
+    Dealers_Turn = false;
 
+    //players.reserve(10);
+    players.resize(value);
+    Dealer.resize(1);
+    Dealer[0].Set_Player_Identity(true);
 
-        cout << "Welcome to Blackjack\n";
-        while (!end) {
-            int option;
-            //phase 1 start game
-            cout << "1) New game\n";
-            cout << "2) Exit\n";
-            cin >> option;
-            if (option == 1)
-            {
-                d.addStandardDeck(Deck);
-                //			cout << "cards successfully added in the following order with a length of " << Deck.size() <<"\n";
-
-                //			for (int i = 1; i < Deck.size(); i++)
-                //				cout << Deck[i].getName() << endl;
-
-                d.Shuffle(Deck); //The deck is being shuffled
-    //			cout << "cards successfully shuffled in the following order with a length of " << Deck.size() << "\n";
-
-    //			for (int i = 1; i < Deck.size(); i++)
-    //				cout << Deck[i].getName() << endl;
+    Deck.addStandardDeck(1);
+    Deck.Shuffle();
 
 
-                //Phase 2 Draw card for player and Dealer
-
-                bool hold = false;
-                bool bust = false;
-                bool dealerBust = false;
-
-                int value = 0;
-                int dealerValue = 0;
-                int hitHold = -1; //determines whether you want to hit, hold or exit
-
-                while (hitHold != 3)
-                {
-                    cout << "The dealer is giving you cards.\n";
-                    d.draw(Deck, Hand, Graveyard, value, bust);
-                    d.draw(Deck, Hand, Graveyard, value, bust);
-                    cout << "The dealer is giving himself cards cards.\n";
-
-                    d.draw(Deck, DealerHand, Graveyard, dealerValue, dealerBust);
-                    d.draw(Deck, DealerHand, Graveyard, dealerValue, dealerBust);
-
-                    //Phase 3 Player may draw or hit
-                    while (!bust && !hold && value != 21) //while the player isn't choosing to stand and hasn't busted and hasn't already drawn 21
-                    {
-
-                        cout << "\n\nYou currently have the following cards:\n";
-                        for (int i = 0; i < Hand.size(); i++)
-                            cout << " " << Hand[i].getName() << endl;
-
-                        cout << "the current value of your hand is " << value << endl;
-                        cout << "The Dealer currently has a\n " << DealerHand[0].getName() << "\n a face down card";
-                        cout << "\nSelect an option :\n1)Hit\n2)Hold\n3)End Game\n";
-                        cin >> hitHold;
-
-
-                        if (hitHold == 1)
-                        {
-                            d.draw(Deck, Hand, Graveyard, value, bust);
-                            cout << "You have drawn a " << Hand[Hand.size() - 1].getName() << endl;
-                        }
-                        else if (hitHold == 2)
-                            hold = true;
-                        else if (hitHold == 3)
-                            return;
-                    }
-
-                    //Phase 4 Dealer will Draw or hit if the player didn't bust and the player hasn't quit the game
-                    if (!bust && value != 21 && hitHold != 3)
-                    {
-                        cout << "The dealer has revealed his face down card is the " << DealerHand[1].getName() << endl;
-                        if (dealerValue == 21)
-                        {
-                            cout << "the dealer hit blackjack.\n";
-                        }
-                        else if (dealerValue >= 17)
-                        {
-                            cout << "The dealer holds on the value " << dealerValue << endl;
-                        }
-                        else {
-                            cout << "The dealer will begin drawing.\n";
-                            int i = 2;
-                            while (dealerValue < 17)
-                            {
-                                d.draw(Deck, DealerHand, Graveyard, dealerValue, dealerBust);
-                                cout << "The dealer has drawn a " << DealerHand[i].getName()<< endl;
-                                i++;
-                            }
-                        }
-                    }
-                    if (value == 21) {
-                        cout << "You have hit blackjack.\n";
-                    }
-                    if (bust)
-                        cout << "You have busted and lost this hand.\n";
-                    else if (dealerBust)
-                        cout << "The dealer has busted and lost this hand.\n";
-                    else {
-                        if (dealerValue > value)
-                            cout << "The dealer has won this hand.\n";
-                        else if (value > dealerValue)
-                            cout << "You have won this hand.\n";
-                    }
-                    cout << endl;
-                    //Places hands into graveyard
-
-                    //make all cards alterable
-                    for (int i = 0; i < Hand.size(); i++)
-                        Hand[i].resetValue();
-                    for (int i = 0; i < DealerHand.size(); i++)
-                        DealerHand[i].resetValue();
-                    Graveyard.reserve(Hand.size() + DealerHand.size());
-                    Graveyard.insert(Graveyard.end(), Hand.begin(), Hand.end());
-                    Graveyard.insert(Graveyard.end(), DealerHand.begin(), DealerHand.end());
-                    while (!Hand.empty())
-                        Hand.erase(Hand.begin());
-                    while (!DealerHand.empty())
-                        DealerHand.erase(DealerHand.begin());
-
-                    value = 0;
-                    dealerValue = 0;
-                    bust = false;
-                    hold = false;
-                    dealerBust = false;
-                    hitHold = -1;
-                }
-
-            }
-            else if (option == 2)
-            {
-                end = true;
-            }
-
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < (int)players.size(); ++j) {
+            Deck.draw(players[j], 0); //Draws the first player and is their first hand
+            if (j != 0)
+                players[j].Hide_Hand();
         }
-*/
+        Deck.draw(Dealer[0], 0);
+    }
 
-    user_card_size = dealers_card_size = 0;
-    //user_cards[0].card_number = random_card;
-    //user_cards[0].card_suit = random_suit;
-    ++user_card_size;
-
-
-
-    //These card images are saved by a 55x80 resolution
-    //Image_File = "Ace of Spades.png";
-    Image_File = "Ace of Spades.png";
-    images[0] = ui->label_1;
-    images[1] = ui->label_2;
-    images[2] = ui->label_3;
-    images[3] = ui->label_4;
-    images[4] = ui->label_5;
-    images[5] = ui->label_6;
-    images[6] = ui->label_7;
-    images[7] = ui->label_8;
-    images[8] = ui->label_9;
-    images[9] = ui->label_10;
-
-
-    //images[0]->setPixmap(QString("BackSide.png"));
-    //ui->label_1->setPixmap(Image_File);
-    //ui->label_3->setPixmap(Image_File);
-    Image_File = "BackSide.png";
-
-    //The table image is saved by a 450x600 resolution
-    Table_Image_File = "Table_Foreground.png";
-
-    //ui->label_2->setPixmap(Image_File);
-    //ui->label_4->setPixmap(Image_File);
-
-    for (int i = 0; i < 10; ++i) {
-        if (i == 0 || i == 2)
-            images[i]->setPixmap(QString("Pictures/Ace of Spades.png"));
-        else if (i == 1 || i == 3)
-            images[i]->setPixmap(QString("BackSide.png"));
-        else
-            images[i]->hide();
+    for (int k = 0; k <= (int)players.size(); ++k) {
+       if (k == (int)players.size()) {
+           cout << "The Dealers Cards are the Following:" << endl;
+           Dealer[0].Display_Hands();
+       }
+       else {
+           cout << "Player #" << k + 1 << " Cards are the Following:" << endl;
+           players[k].Display_Hands();
+       }
 
     }
 
-
-    ui->Table->setPixmap(Table_Image_File);
-
-    //ui->label_5->hide();
-    //ui->label_6->hide();
-    //ui->label_7->hide();
-    //ui->label_8->hide();
-    //ui->label_9->hide();
-    //ui->label_10->hide();
+    Players_Turn = "It is currently player #";
+    Players_Turn += QString::number(current_player_number+1);
+    Players_Turn += "'s Turn\n";
+    ui->textBrowser->setText(Players_Turn);
 
 }
 
@@ -223,34 +76,77 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::Show_Card(string PictureName, int label_number) {
-     QString CardName = QString::fromStdString(PictureName + ".png");
-     images[label_number]->setPixmap(CardName);
-}
-
-void MainWindow::Display_Cards(vector <Card> &Hand, vector <Card> &DealerHand ) {
-    for (int i = 0; i < 4; ++i) {
-       if (i > 1)
-            cout << "The dealers card number " << i-1 << " is "
-                 << DealerHand[i-2].getName() << endl;
-       else
-           cout << "The Players card number " << i+1
-                << " is " << Hand[i].getName() << endl;
-    }
-}
-
-void MainWindow::on_pushButton_clicked()
+// This needs to check the hand size and needs to allow them to hit based on how many hands they have
+void MainWindow::on_Hit_Button_clicked()
 {
-
-    if (Image_File == "BackSide.png"){
-        Image_File = "Pictures/Ace of Spades.png";
+    if (Dealers_Turn)
+        Deck.draw(Dealer[0], 0);
+    else {
+        Deck.draw(players[current_player_number], current_hand_number);
+    }
+    if (Dealers_Turn) {
+        if (Dealer[0].Is_Busted(0))
+            Dialog_Text = "You Busted with a value of " + QString::number(Dealer[0].Get_Current_Hand_value(0)) + "\n";
+        else
+            Dialog_Text = "Your current hand value is " + QString::number(Dealer[0].Get_Current_Hand_value(0)) + "\n";
     }
 
     else {
-        Image_File = "BackSide.png";
+        if (players[current_player_number].Is_Busted(0))
+            Dialog_Text = "You Busted with a value of " + QString::number(players[current_player_number].Get_Current_Hand_value(current_hand_number)) + "\n";
+        else
+            Dialog_Text = "Your current hand value is " + QString::number(players[current_player_number].Get_Current_Hand_value(current_hand_number)) + "\n";
     }
 
-    ui->label_2->setPixmap(Image_File);
-    ui->label_4->setPixmap(Image_File);
+    Dialog_Text = Players_Turn + Dialog_Text;
+
+    ui->textBrowser->setText(Dialog_Text);
+
 }
 
+void MainWindow::on_Stay_Button_clicked()
+{
+    cout << "Players size is " << players.size() << endl;
+    if (Dealers_Turn == true) {
+        Dealers_Turn = false;
+        current_player_number = 0;
+        Players_Turn = "It is currently player #1's turn\n";
+        players[players.size()-1].Hide_Hand();
+        players[0].Show_Hand();
+    }
+
+    else if (int(players.size()) == (current_player_number + 1) && (players[current_player_number].Get_Hand_Count()-1 == current_hand_number)) {
+        Dealers_Turn = true;
+        Players_Turn = "It is currently the Dealers's turn\n";
+        current_hand_number = 0;
+    }
+    else {
+        if (current_hand_number == (int)players[current_player_number].Get_Hand_Count()-1) {
+            current_hand_number = 0;
+        players[current_player_number].Hide_Hand();
+        ++current_player_number;
+
+        players[current_player_number].Show_Hand();
+        Players_Turn = "It is currently player #";
+        Players_Turn += QString::number(current_player_number+1);
+        Players_Turn += "'s Turn\n";
+        }
+        else {
+            ++current_hand_number;
+            Players_Turn = "It is currently player #";
+            Players_Turn += QString::number(current_player_number+1);
+            Players_Turn += "'s Turn\nHand number " + QString::number(current_hand_number+1);
+        }
+   }
+    ui->textBrowser->setText(Players_Turn);
+}
+
+void MainWindow::on_Split_Button_clicked()
+{
+    players[current_player_number].Split();
+    current_hand_number = 0;
+
+    //if (int(players.size()) == (current_player_number + 1))
+    //        current_player_number = 0;
+
+}
--- mainwindow.h
+++ mainwindow.h
@@ -3,8 +3,11 @@
 
 #include <QMainWindow>
 #include <QLabel>
+#include <QString>
 #include <vector>
 #include "card.h"
+#include "player.h"
+#include "deck.h"
 
 using namespace std;
 
@@ -18,24 +21,61 @@ class MainWindow : public QMainWindow
 
 public:
     explicit MainWindow(QWidget *parent = 0);
-    void Show_Card(string PictureName, int label_number);
-    void Display_Cards(vector <Card>&Hand, vector <Card>&DealerHand);
+
+    //Could use two ints for Deck ammounts and player ammount
+    void Create_Deck() {return;}
+
+    // Starts the game over. Removes info about players, clears hands, and deletes all cards
+    void New_Game() {return;}
+
+    // Could allow the new game types to be accessed and returns a bool if done sucessfully or not
+    bool Change_Game_Type(string game_type) {return false;}
+
+    // You pass the total number of players and returns a bool if done sucessfully or not
+    bool Add_Players(int total_players) {return false;}
+
+    // For the betting part of this program
+    bool Add_money() {return false;}
+
+    //Clears out their money so they could "Cash out"
+    bool Cash_out(string username) {return false;}
+
+    // Allows the player to change to a different username
+    bool Change_user(string username) {return false;}
+
+
+
     ~MainWindow();
 
 
 private slots:
-    void on_pushButton_clicked();
+    // Whenever the user clicks on Hit, it will call this function
+    void on_Hit_Button_clicked();
+
+    // Whenever the user clicks on Stay, it will call this function
+    void on_Stay_Button_clicked();
+
+    // Whenever the user clicks on Split, it will call this function
+    void on_Split_Button_clicked();
 
 private:
-
     Ui::MainWindow *ui;
 
+    // Stores all information about Player(s)
+    vector<Player> players;
 
-    QLabel *images[10];
-    QString Image_File;
-    QString Table_Image_File;
-    int user_card_size;
-    int dealers_card_size;
+    // Stores information about the dealer. MUST REMAIN A VECTOR
+    vector<Player> Dealer;
+
+    bool Dealers_Turn;
+    int current_player_number;
+    int current_hand_number;
+
+    QString Title_Message;
+    QString Players_Turn;
+    QString Dialog_Text;
+
+    deck Deck;
 
 };
 
